331e41b2b33bfd5d7ea05145855870972c0c9947
juanjosalaz20 <juanjosalazar2000@gmail.com>
2025-07-15 20:10:56 -0500

    CLASE 15/7/25

--- controllers/UsuarioController.js
+++ controllers/UsuarioController.js
@@ -5,3 +5,14 @@ exports.obtenerUsuarios= async (req, res)=>{
     const usuarios = await Usuario.findAll()
     res.json(usuarios)
 };
+
+exports.obtenerUsuariosporId= async (req, res)=>{
+    const id=parseInt(req.params.id);
+
+    const usuario = await Usuario.findByPk(id)
+
+    if (!usuario){
+        return res.status(404).json({ mensaje: 'Usuario no encontrado'});
+    }
+    res.json(usuario)
+};
--- routes/UsuarioRoutes.js
+++ routes/UsuarioRoutes.js
@@ -4,4 +4,22 @@ const controlador = require('../controllers/UsuarioController');
 
 router.get('/usuarios/todos', controlador.obtenerUsuarios);
 
+router.get('/usuarios/buscar/:id', controlador.obtenerUsuariosporId )
+
 module.exports=router;
+
+// //ruta para obtener un usuario especifico
+// app.get('/usuarios/buscar/:id', (req,res)=>{
+//     const id=parseInt(req.params.id);
+//     //console.log(typeof(id))
+//     const usuario=usuarios.find(user=>user.id===id)
+//     //console.log(usuario)
+
+//     if(!usuario){
+//         return res.status(404).json({
+//             mensaje:'Usuario no encontrado'
+//         })
+//     }
+
+//     res.json(usuario)
+// })
